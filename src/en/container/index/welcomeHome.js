73880ecc289189898fab41cdf925e6b6536d92fb
aras armani <aras.armani2014@gmail.com>
2018-12-01 21:06:23 +0300

    review drop list

--- src/en/container/index/welcomeHome.js
+++ src/en/container/index/welcomeHome.js
@@ -40,19 +40,16 @@ class WelcomeHome extends Component {
             <div className="container-fluid bg-gradient-red pb100">
                 <div className="container pb50">
                     <h1 className="center pb50 pt50">Welcome home — anywhere, anytime</h1>
-
-
                             <div style={{ margin: 20 }}>
                                 <div style={{ height: 100,  }}/>
                                 <div>
-                                <Dropdown
-                                    trigger={['click']}
-                                    overlay={menu}
-                                    animation="slide-up" 
-                                     
-                                >
-                                    <div className="drop-down-list">Person <img src={arrow} style={{paddingLeft:10, paddingTop:5, height:11, width:11, float:'right' }} /> </div>
-                                </Dropdown>
+                                    <Dropdown
+                                        trigger={['click']}
+                                        overlay={menu}
+                                        animation="slide-up" 
+                                    >
+                                        <div className="drop-down-list">Person <img src={arrow} style={{paddingLeft:10, paddingTop:5, height:11, width:11, float:'right' }} /> </div>
+                                    </Dropdown>
                                 </div>
                             </div>
 
