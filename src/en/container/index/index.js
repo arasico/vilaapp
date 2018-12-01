35312edfd0f56a5312924320b4306a444e281db6
aras armani <aras.armani2014@gmail.com>
2018-12-01 12:57:25 +0300

    index page is added

---
+++ src/en/container/index/index.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+class Index extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                <p>Hello Index vilaap</p>
+            </div>
+         );
+    }
+}
+ 
+export default Index;
