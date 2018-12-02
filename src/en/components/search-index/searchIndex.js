3765500cafb2ff69d073bc907009e11aae4069cf
aras armani <aras.armani2014@gmail.com>
2018-12-02 09:26:34 +0300

    fixed drop donw

--- src/en/components/search-index/searchIndex.js
+++ src/en/components/search-index/searchIndex.js
@@ -1,11 +1,32 @@
 import React,{Component} from 'react';
 import DatePicker from "react-datepicker";
- 
 import "react-datepicker/dist/react-datepicker.css";
+import Dropdown from 'rc-dropdown';
+import Menu, { Item as MenuItem, Divider } from 'rc-menu';
+import 'rc-dropdown/assets/index.css';
+import arrow from '../../../assets/icons/arrow-down.svg'
+
 import './style.css'
 
  
  
+
+
+function onSelect({ key }) {
+    console.log(`${key} selected`);
+  }
+  
+  const menu = (
+    <Menu onSelect={onSelect}>
+       
+      <MenuItem key="1" style={{fontSize: 16}}>1 person</MenuItem>
+      <Divider />
+      <MenuItem key="2">2 persons</MenuItem>
+      <Divider />
+      <MenuItem key="3">3 persons</MenuItem>
+    </Menu>
+  );
+
 
 
 
@@ -30,15 +51,13 @@ class SearchIndex extends Component {
             <div className="search-bx-container">
                 <ul>
                     <li>
-           
-
-                        <select className="drop-down">
-                            <option>Select City</option>
-                            <option>Tehran</option>
-                            <option>Babolsar</option>
-                            <option>Amol</option> 
-  
-                        </select>
+                        <Dropdown
+                            trigger={['click']}
+                            overlay={menu}
+                            animation="slide-up" 
+                        >
+                            <div className="drop-down-list">Select City <img src={arrow} style={{paddingLeft:10, paddingTop:5, height:11, width:11, float:'right' }} /> </div>
+                        </Dropdown>
                     </li>
                     
                     <li>
@@ -59,17 +78,13 @@ class SearchIndex extends Component {
                     </li>
                     
                     <li>
-
-                        <select className="drop-down">
-                            <option>1 Person</option>
-                            <option>2 Person</option>
-                            <option>3 Person</option>
-                            <option>5 Person</option> 
-                            <option>6 Person</option> 
-                            <option>7 Person</option> 
-                            <option>8 Person</option> 
-
-                        </select>
+                        <Dropdown
+                            trigger={['click']}
+                            overlay={menu}
+                            animation="slide-up" 
+                        >
+                            <div className="drop-down-list">Person <img src={arrow} style={{paddingLeft:10, paddingTop:5, height:11, width:11, float:'right' }} /> </div>
+                        </Dropdown>
                     </li> 
                 </ul>
                 <button type="button" className="btn-search">Search</button>
