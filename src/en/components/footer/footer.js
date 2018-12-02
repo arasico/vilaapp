948c87b0746a81369314fd192272a479bb384298
aras armani <aras.armani2014@gmail.com>
2018-12-02 10:24:39 +0300

    logo added

--- src/en/components/footer/footer.js
+++ src/en/components/footer/footer.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-
+import './style.css'
+import LogoBlack from '../../../assets/img/logo-vilaapp-000.svg'
 
 class FooterComponent extends Component {
     constructor(props) {
@@ -8,8 +9,12 @@ class FooterComponent extends Component {
     }
     render() { 
         return ( 
-            <div>
-                <p>Footer is heere</p>
+            <div className="container-fluid footer-color-black ">
+                <div className="container center pt50 pb50">
+                    <img src={LogoBlack} />
+                    <hr />
+                    <h1>Footer is heere</h1>
+                </div>
             </div>
          );
     }
