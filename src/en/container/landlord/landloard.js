bd18a747c6088dd1f983655ffc5261fad91994f1
aras armani <aras.armani2014@gmail.com>
2018-12-02 15:23:48 +0300

    vila app is fixed

--- src/en/container/landlord/landloard.js
+++ src/en/container/landlord/landloard.js
@@ -1,4 +1,6 @@
 import React,{Component} from 'react';
+import { NavLink  } from 'react-router-dom';
+
 import './style.css';
 
 
@@ -9,12 +11,36 @@ class LandLoard extends Component {
     }
     render() { 
         return ( 
-            <div className="container-fluid " >
-                <div id="#furnished" className="container pt100 ">
-                    <h1>Rent out your </h1>
-                    <h1>furnished apartment</h1>
+            <div>
+
+                <div className="container-fluid pb100 pt50" >
+                    <div  className="container pt100 pb100 furnished ">
+                        <h1 className="pt100">Rent out your </h1>
+                        <h1 className="pb100">furnished apartment</h1>
+
+                        <h3 className="pb50">
+                            VilaApp makes it easy and convenient to reach thousands of reliable tenants that are looking for a furnished apartment. <br />List once - rent out safely forever
+                        </h3>
+
+                        <NavLink to="/#" className="btn  btn-green">Become a landlord</NavLink>
+                    </div>
                 </div>
-            </div>
+
+                    <div className="container-fluid vilaapp-way" >
+                        <div  className="container pt100  wilaap-way-container  ">
+                            <h3>Rent out your apartment</h3>
+                            <h1>the VilaApp way</h1>
+                        </div>
+                    </div>
+
+                    <div className="container-fluid vilaapp-way-continu" >
+                    
+                    </div>
+
+
+
+
+        </div>
          );
     }
 }
