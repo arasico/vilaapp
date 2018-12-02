fd00951de5697126209d5d3acd97b4eba7bb7998
aras armani <aras.armani2014@gmail.com>
2018-12-02 10:00:23 +0300

    all images of city is fixed

--- src/en/container/index/welcomeHome.js
+++ src/en/container/index/welcomeHome.js
@@ -7,6 +7,9 @@ import 'rc-dropdown/assets/index.css';
 import './index.css'
 import arrow from '../../../assets/icons/arrow-down.svg'
 import imgKish from '../../../assets/img/kish.jpg'
+import imgTehran from '../../../assets/img/tehran.jpg'
+import imgShiraz from '../../../assets/img/shiraz.jpg'
+import imgEsfehan from '../../../assets/img/esfehan.jpg'
 
 
 function onSelect({ key }) {
@@ -44,15 +47,23 @@ class WelcomeHome extends Component {
                  
                     <div className="images-container-index">
                         <div className="index-box-image">
-                        <p className="title-image">Kish,Iran</p>
-                          <img src={imgKish} style={{width:'100%', height:'100%'}}  />
-                         
+                            <p className="title-image">Kish,Iran</p>
+                            <img src={imgKish} style={{width:'100%', height:'100%'}}  />
                         </div>
                         <div className="two-images-container">
-                            <div className="index-box-image-50"></div>
-                            <div className="index-box-image-50"></div>
+                            <div className="index-box-image-50">
+                                <p className="title-image">Shiraz,Ira n</p>
+                                <img src={imgShiraz} style={{width:'100%', height:'100%'}}  />    
+                            </div>
+                            <div className="index-box-image-50">
+                                <p className="title-image">Tehran,Iran</p>
+                                <img src={imgTehran} style={{width:'100%', height:'100%'}}  />
+                            </div>
                         </div>
-                        <div className="index-box-image"></div>
+                        <div className="index-box-image">
+                            <p className="title-image">Esfehan,Iran</p>
+                            <img src={imgEsfehan} style={{width:'100%', height:'100%'}}  />
+                        </div>
 
                     </div>
 
