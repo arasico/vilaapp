c27c5e28299b3b85de39be119cf9a0b2c426fcef
aras armani <aras.armani2014@gmail.com>
2018-12-03 16:50:31 +0300

    tabs is fixeddd

--- src/en/container/create/create.js
+++ src/en/container/create/create.js
@@ -1,22 +1,45 @@
 import React,{Component} from 'react';
 
+ 
 import './style.css';
 
-
+ 
 
 class CreateComponent extends Component {
     constructor(props) {
-        super(props);
-        this.state = {  }
+        super(props); 
+      }
+    
+   
+      openCity(cityName) {
+        var i;
+        var x = document.getElementsByClassName("city");
+        for (i = 0; i < x.length; i++) {
+           x[i].style.display = "none";  
+        }
+       console.log(cityName)
+        document.getElementById(cityName).style.display = "block";  
     }
+      
+
     render() { 
         return ( 
             <div className="container-fluid">
                <div className="container pt100">
                    <div className="tabs-menu-container">
-
+                        <div className="menu-item" id="address" onClick={() => this.openCity('aras')}>1</div>
+                        <div className="menu-item" id="info" onClick={() => this.openCity('javid')}>2</div>
+                        <div className="menu-item" id="image" onClick={() => this.openCity('yasi')}>3</div>
                    </div>
                </div>
+
+               <div className="container">
+                    <div id="aras" className="basic-tab city"><p>adress</p></div>
+                    <div id="javid" className="basic-tab city"><p>info</p></div>
+                    <div id="yasi" className="basic-tab city"><p>images</p></div>
+
+               </div>
+ 
             </div>
          );
     }
