7e4d5fee220dcb0e113925bb0f2b4da9e82fa8a7
aras armani <aras.armani2014@gmail.com>
2018-12-04 12:32:34 +0300

    search-box is fixed

--- src/en/container/create/create.js
+++ src/en/container/create/create.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import { Map, Marker, MarkerLayout } from 'yandex-map-react';
 
  
 import './style.css';
@@ -8,6 +9,7 @@ import './style.css';
 class CreateComponent extends Component {
     constructor(props) {
         super(props); 
+
       }
     
    
@@ -27,14 +29,33 @@ class CreateComponent extends Component {
             <div className="container-fluid">
                <div className="container pt100">
                    <div className="tabs-menu-container">
-                        <div className="menu-item" id="address" onClick={() => this.openCity('aras')}>1</div>
+                        <div className="menu-item" id="address" onClick={() => this.openCity('aras')}>
+                        <div className="tabs-circle-container">
+                            <span>1</span>
+                        </div>
+                        </div>
                         <div className="menu-item" id="info" onClick={() => this.openCity('javid')}>2</div>
                         <div className="menu-item" id="image" onClick={() => this.openCity('yasi')}>3</div>
                    </div>
                </div>
 
-               <div className="container">
-                    <div id="aras" className="basic-tab city"><p>adress</p></div>
+               <div className="container-fluid">
+                    <div id="aras" className="basic-tab city">
+                        <Map width="100%"  onAPIAvailable={function () { console.log('API loaded'); }} center={[40.754734, 40.583314]} zoom={10}>
+                            <Marker lat={40.783379} lon={40.775575}  lang={'tr-TR'} >
+                                <MarkerLayout>
+                                    <div style={{borderRadius: '50%', overflow: 'hidden'}}>
+                                        <img src="http://loremflickr.com/80/80"/>
+                                    </div>
+                                </MarkerLayout>
+                            </Marker>
+                        </Map>
+
+
+                        <div className="map-search-container">
+
+                        </div>
+                    </div>
                     <div id="javid" className="basic-tab city"><p>info</p></div>
                     <div id="yasi" className="basic-tab city"><p>images</p></div>
 
