a6cb3bb8c83b3b2e4560cc2905dcba2ebe8acbda
aras armani <aras.armani2014@gmail.com>
2018-12-13 08:35:08 +0300

    add text area component

--- src/en/container/contactUs/contactUs.js
+++ src/en/container/contactUs/contactUs.js
@@ -2,6 +2,8 @@ import React , { Component }  from 'react';
 import './contactUs.css'
 
 import SubTitle from '../../components/common/subTitle/subTitle'
+import InputText from '../../components/commonInput/InputGroup'
+import InputTextArea from '../../components/commonInput/InputTextArea'
 
 
 class ContactUsComponent extends Component {
@@ -12,8 +14,39 @@ class ContactUsComponent extends Component {
     render() { 
         return ( 
             <div className="container-fluid">
-                <div className="container">
+                <div className="container pt100">
                     <SubTitle label="Contact us" />
+
+                    <form>
+                        <InputText
+                            type={'text'} name="name"
+                            placeHolder={'Name'}
+                            changed={this.changedHandler}
+                            error={'name is error'}
+                        /> 
+
+                              <InputText
+                            type={'text'} name="email"
+                            placeHolder={'Email'}
+                            changed={this.changedHandler}
+                            error={'Email is error'}
+                        /> 
+
+                              <InputText
+                            type={'text'} name="phone"
+                            placeHolder={'Phone Number'}
+                            changed={this.changedHandler}
+                            error={'Phone is error'}
+                        /> 
+
+                              <InputTextArea
+                            type={'text'} name="message"
+                            placeHolder={'Message'}
+                            changed={this.changedHandler}
+                            error={'name is error'}
+                        /> 
+                        
+                    </form>
                     
                 </div>
             </div>
