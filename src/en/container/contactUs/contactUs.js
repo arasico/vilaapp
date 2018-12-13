10d6b6f8e38d9939deb5e64e357e793730a9dc20
aras armani <aras.armani2014@gmail.com>
2018-12-13 08:07:36 +0300

    contact us is cretate

---
+++ src/en/container/contactUs/contactUs.js
@@ -0,0 +1,18 @@
+import React , { COmponent }  from 'react';
+
+
+class ContactUsComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                <p>Contact us</p>
+            </div>
+         );
+    }
+}
+ 
+export default ContactUsComponent;
