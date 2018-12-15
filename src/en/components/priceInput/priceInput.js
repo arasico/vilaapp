37002fd7780aa25eb04e539ee2f37c094dcb7df1
aras armani <aras.armani2014@gmail.com>
2018-12-15 18:19:05 +0300

    price input component create

---
+++ src/en/components/priceInput/priceInput.js
@@ -0,0 +1,24 @@
+
+/*
+Price input 3.000.000 Toman example
+*/
+
+import React,{Component} from 'react';
+import './priceInput.css';
+
+
+class PriceInput extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                <p>Price input 30.0.0.0.0</p>
+            </div>
+         );
+    }
+}
+ 
+export default PriceInput;
