ac044b906d1ccfb087796d0c070aead1b06e1555
aras armani <aras.armani2014@gmail.com>
2019-01-10 14:56:12 +0300

    change it

--- src/en/components/root.js
+++ src/en/components/root.js
@@ -1,9 +1,7 @@
 import React, {Component} from 'react';
 import Header from "./header/header";
 import Footer from "./footer/footer";
-// import Header from "./header/HeaderEnglish";
 
-  
 export default class Root extends Component {
     constructor(props) {
         super(props);
