72d3aa322963bd19ef1495769ff70ba199337ada
aras armani <aras.armani2014@gmail.com>
2019-01-15 20:05:29 +0300

    condishtion for login and after login

--- src/en/components/root.js
+++ src/en/components/root.js
@@ -3,6 +3,9 @@ import Header from "./header/header";
 import Footer from "./footer/footer";
 import HeaderComponentAfterLogin from './../components/afterLogin/header/header'
 
+
+const token = 2;
+
 export default class Root extends Component {
     constructor(props) {
         super(props);
@@ -12,8 +15,8 @@ export default class Root extends Component {
         return ( 
             <div>
                 <div>
-                     {/* <Header />  */}
-                     <HeaderComponentAfterLogin/>
+                    {token === 1 ?  <Header /> :  <HeaderComponentAfterLogin/> }
+                                       
                 </div>
                 <div> 
                     {this.props.children}
