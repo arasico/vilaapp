893b22df69670b5caa95e499c8d345ab20290249
aras armani <aras.armani2014@gmail.com>
2019-01-16 22:38:17 +0300

    form fixed

--- src/en/container/afterLogin/setting/setting.js
+++ src/en/container/afterLogin/setting/setting.js
@@ -1,18 +1,57 @@
 import React, {Component} from 'react';
 
+//
+//
+//
+import SubTitle from '../../../components/common/subTitle/subTitle';
+import Input from '../../../components/commonInput/InputGroup'
+import Button from '../../../components/Button/Button'
 
 class Stting extends Component {
     constructor(props) {
         super(props);
         this.state = {  }
     }
+
+
+
     render() { 
         return ( 
-            <div>
-                <p>Setting</p>
+            <div className="container-fluid pb100 pt50">
+                   <div  className="container pt100 pb100  ">
+                        <SubTitle label="Setting" />
+
+                        <form>
+                            <Input 
+                                type={'password'} 
+                                name={'currentPassword'}
+                                placeHolder={'Current Password'}
+                                changed={this.changedHandler}
+                                error={this.state.forgetEmailError}
+                            />
+                            <Input 
+                                type={'password'} 
+                                name={'newPassword'}
+                                placeHolder={'New Password'}
+                                changed={this.changedHandler}
+                                error={this.state.forgetEmailError}
+                            />
+                            <Input 
+                                type={'newPasswordAgain'} 
+                                name={'currentPassword'}
+                                placeHolder={'New Password again'}
+                                changed={this.changedHandler}
+                                error={this.state.forgetEmailError}
+                            />
+
+                        </form>
+                   </div>
             </div>
          );
     }
 }
  
 export default Stting;
+
+
+ 
