9b4fdab36bce01c0f01d349493e51792237c14a2
aras armani <aras.armani2014@gmail.com>
2019-01-16 22:21:31 +0300

    setting is added

---
+++ src/en/container/afterLogin/setting/setting.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+class Stting extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                <p>Setting</p>
+            </div>
+         );
+    }
+}
+ 
+export default Stting;
